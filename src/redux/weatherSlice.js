ec4966ff3057541074b226d0498cfe84c7e2599b
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-15 22:46:12 +0300

    add redux files

---
+++ src/redux/weatherSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const weatherSlice = createSlice({
+  name: 'weather',
+  initialState: {},
+  reducers: {}
+})
+
+export default weatherSlice.reducer;
