6f5259d560586dae719fcb3074ba3a58ba51383c
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-16 14:20:04 +0300

    update nearDaysWeather js and sass file

--- src/Container/NearDaysWeather.js
+++ src/Container/NearDaysWeather.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 function NearDaysWeather() {
     return (
-        <div>
-            near days weather
+        <div className="nearDaysWeather">
+            <div className="nearDaysWeather__information">
+                <span className="nearDaysWeather__information__day">TUE</span>
+                <img src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" className="nearDaysWeather__information__icon" />
+                <span className="nearDaysWeather__information__temperature">24°C</span>
+            </div>
+            <div className="nearDaysWeather__information">
+                <span className="nearDaysWeather__information__day">TUE</span>
+                <img src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" className="nearDaysWeather__information__icon" />
+                <span className="nearDaysWeather__information__temperature">24°C</span>
+            </div>
+            <div className="nearDaysWeather__information">
+                <span className="nearDaysWeather__information__day">TUE</span>
+                <img src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" className="nearDaysWeather__information__icon" />
+                <span className="nearDaysWeather__information__temperature">24°C</span>
+            </div>
+            <div className="nearDaysWeather__information">
+                <span className="nearDaysWeather__information__day">TUE</span>
+                <img src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" className="nearDaysWeather__information__icon" />
+                <span className="nearDaysWeather__information__temperature">24°C</span>
+            </div>
+            <div className="nearDaysWeather__information">
+                <span className="nearDaysWeather__information__day">TUE</span>
+                <img src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" className="nearDaysWeather__information__icon" />
+                <span className="nearDaysWeather__information__temperature">24°C</span>
+            </div>
         </div>
     )
 }
