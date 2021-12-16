e02f5fa3c2e8d414e9d4b6b64f69e2d990af941b
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-16 14:03:20 +0300

    update todayWeather.js, todayWeather.scss and variables.scss

--- src/Container/TodayWeather.js
+++ src/Container/TodayWeather.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 function TodayWeather() {
     return (
-        <div>
-            today weather
+        <div className="todayWeather">
+            <div className="todayWeather__weather">
+                <img className="todayWeather__weather__icon" src="https://i.pinimg.com/originals/53/22/c2/5322c2cad533e12e552d0dfdc89b4c25.png" alt="sunny" />
+                <span className="todayWeather__weather__iconName">Sunny</span>
+            </div>
+            <span className="todayWeather__temperature">9°C</span>
+            <div className="todayWeather__status">
+                <span className="todayWeather__status__wind">Wind: 11 kmph</span>
+                <span className="todayWeather__status__precip">Precip: 00 mm</span>
+                <span className="todayWeather__status__pressure">Pressure: 1012 mb</span>
+            </div>
         </div>
     )
 }
