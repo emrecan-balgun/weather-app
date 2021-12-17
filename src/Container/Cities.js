efdb79e949e10636c59372494e73aa199da05d7c
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-17 19:28:45 +0300

    refactor cities.js and cities.scss

--- src/Container/Cities.js
+++ src/Container/Cities.js
@@ -1,48 +1,14 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Select from 'react-select'
+import react from 'react';
 
 function City() {
-   const [data, setData] = useState([]);
-
-    const headers = {
-        "Authorization": process.env.REACT_APP_API_KEY,
-        "Accept": "application/json"
-    }
-        
-    useEffect(() => {
-        const res = axios.get('https://www.universal-tutorial.com/api/countries', {
-        headers: headers
-        })
-        .then(response => {
-            setData(response.data);
-            })
-    }, [])
-
-
-    const option = [];
-
-    if(data != null) {
-        data.map((item) => {
-            const obj = new Object();
-            obj.value = `${item.country_name}`
-            obj.label = `${item.country_name}`
-            option.push(obj)
-        }        
-        )
-    }
-
-    function changeHandler(value) {
-        console.log(value.label);
-    }
-
     return (
         <div className="cities">
-            <Select
-                onChange={changeHandler}
-                options={option}
-                isSearchable
-                isClearable
+            <input 
+                type="text"
+                // value={text}
+                onChange={(e) => console.log(e.target.value)}
+                className="cities__input"
+                placeholder="Please enter a city name.."
             />
         </div>
     )
