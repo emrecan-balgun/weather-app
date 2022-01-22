707754ccf21daa9cdc752332516b41dc40050b8d
Emrecan Balgün <deathlygrave@gmail.com>
2022-01-23 01:36:02 +0300

    fix for bug (a problem with positionstack)

--- src/Container/NearDaysWeather.js
+++ src/Container/NearDaysWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { latitude, longitude } from '../redux/weatherSlice';
+import { latitude, longitude, city } from '../redux/weatherSlice';
 import { nanoid } from 'nanoid';
 import Moment from 'react-moment';
 import 'moment-timezone';
@@ -9,6 +9,7 @@ import 'moment-timezone';
 function NearDaysWeather() {
     const lat = useSelector(latitude);
     const lon = useSelector(longitude);
+    const cityName = useSelector(city);
 
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -17,7 +18,7 @@ function NearDaysWeather() {
 
     useEffect(() => {
         if(lat !== '') {
-            axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+            axios(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
             .then(response => setData(response.data.daily))
             .catch(e => console.log(e))
             .finally(() => setIsLoading(false))
@@ -39,7 +40,7 @@ function NearDaysWeather() {
                 : newData.map(data => 
                     <div className="nearDaysWeather__information" key={nanoid()}>
                         <span className="nearDaysWeather__information__day"><Moment format="ddd">{data.dt*1000}</Moment></span>
-                        <img src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt={data.weather[0].description} className="nearDaysWeather__information__icon" />
+                        <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt={data.weather[0].description} className="nearDaysWeather__information__icon" />
                         <div className="nearDaysWeather__information__temperature">
                             <span className="nearDaysWeather__information__temperature__morn">{Math.ceil(data.temp.morn)}°C</span>
                             <span className="nearDaysWeather__information__temperature__seperator">/</span>
